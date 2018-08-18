b2d16225f556b845d759aa15622fff50ab19680a
pokias <mmlumme@gmail.com>
2018-08-19 00:50:36 +0300

    Line Remove
    Remove line working, but not with cookies.  needs keep same index as array/cookies and in loading too

--- javascript.js
+++ javascript.js
@@ -3,14 +3,16 @@
 // Keksit säilyvät kyseisen istunnon ajan
 // TODO mahdollisuus tallentaa muistilista käyttäjän koneelle
 // TODO Lisätä oma Muistilista
+		var IdNumber = 0; //Tietyn linen poistamista varten
 		var CookieArray = [];
+// kuvalle ID, laita sama ID Linelle, klikkaa poisto kuvaa -> hae sen id tuotos ja poista kuva että line
 
 
 		function SetCookie(CookieArray){
 			document.cookie = CookieArray.join();
 		}
 
-// Valmis kirjasta sanitointiin.
+// Valmis kirjasto sanitointiin.
 function getCookie(ca) {
 			var ca = document.cookie.split(",");
     for(var i = 0; i < ca.length; i++) {
@@ -30,13 +32,26 @@ function checkCookie() {
  }
 };
 
+function SaveCookies() {
+	var blob = new Blob(CookieArray, {type: "text/plain;charset=utf-8"});
+	var blobUrl = URL.createObjectURL(blob);
+	document.getElementById("Download").href = blobUrl
+	document.getElementById("Download").download = "Muistilista.txt"
+}
+
 function DeleteCookies(){
   document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 	location.reload();
 }
 
-
 function AddLineFunction(listitem) {
+		// TODO Fiksumpi tapa
+		var TrashPicture = document.createElement("IMG");
+		TrashPicture.setAttribute("src", "m.jpg");
+		TrashPicture.setAttribute("alt", "Remove Line");
+		TrashPicture.setAttribute("class", "RemoveIcon");
+		TrashPicture.setAttribute("onclick","RemoveLine(this.id)");
+
 		var listitem = document.getElementById("List").value;
 		CookieArray.push(listitem);
 		document.getElementById("List").value = "";
@@ -44,17 +59,21 @@ function AddLineFunction(listitem) {
     var node = document.createElement("LI");
     var textnode = document.createTextNode(listitem);
     node.appendChild(textnode);
-    document.getElementById("list_output").appendChild(node);
+		node.appendChild(TrashPicture).setAttribute("id", IdNumber);
+    document.getElementById("list_output").appendChild(node).setAttribute("id",IdNumber);
+		IdNumber++;
 		SetCookie(CookieArray);
 }
 
-function SaveCookies() {
-	var blob = new Blob(CookieArray, {type: "text/plain;charset=utf-8"});
-	var blobUrl = URL.createObjectURL(blob);
-	document.getElementById("Download").href = blobUrl
-	document.getElementById("Download").download = "Muistilista.txt"
-
+function RemoveLine(id){
+	alert(id);
+	var elem = document.getElementById(id);
+	elem.parentNode.removeChild(elem);
 }
+
+
+
+
 
 // Enterillä lähettää inputin
 // Ei toimi tällä hetkellä
